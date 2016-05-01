166ed308e4531f024209f00aa67f20ad8503bb06
Mohammad Alow <md-alow@hotmail.com>
2016-05-02 01:50:59 +0300

    Split rspec configuration into separate files.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,9 +5,6 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-require 'pundit/rspec'
-
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 include ApplicationHelper
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -25,18 +22,13 @@ include ApplicationHelper
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
-
-  config.include Devise::TestHelpers, type: :controller
-  config.include Devise::TestHelpers, type: :view
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
