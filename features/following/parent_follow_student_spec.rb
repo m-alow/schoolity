8d9de29476730a0ec3316c1ce7e73c8f28f70d6f
Mohammad Alow <md-alow@hotmail.com>
2016-06-06 22:29:08 +0300

    Refine following student feature test.

--- features/following/parent_follow_student_spec.rb
+++ features/following/parent_follow_student_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 require_relative '../capybara'
 
 feature 'following a student' do
-  let(:school) { create(:active_school) }
   let(:parent) { create(:user) }
-  let(:student) { create(:student, school: school) }
-  let(:following_code) { FollowingCode.make! student }
+  let(:student) { create(:student) }
+  let!(:following_code) { FollowingCode.make! student }
 
   scenario 'parent follows a student with a valid following code' do
     sign_in_user parent
@@ -26,10 +25,7 @@ feature 'following a student' do
   scenario 'parent cannot follow a student with an expired following code' do
     sign_in_user parent
 
-    following_code
-
     Timecop.travel(Time.now + 100.hours) do
-      puts 'shit' if  following_code.expired?
       NewFollowingForm.
         visit_page.
         fill_in_with(
@@ -37,8 +33,7 @@ feature 'following a student' do
           student_full_name: student.full_name,
           relationship: 'Father').
         submit
-
-      expect(page).not_to have_content 'successfully followed'
     end
+    expect(page).not_to have_content 'successfully followed'
   end
 end
