b043869b6ccc9d4d7f4ce0306d4bd284fe797d4f
Mohammad Alow <md-alow@hotmail.com>
2016-06-06 05:51:05 +0300

    Remove authorization methods from application policy.

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -13,34 +13,6 @@ class ApplicationPolicy
     true
   end
 
-  def index?
-    false
-  end
-
-  def show?
-    scope.where(:id => record.id).exists?
-  end
-
-  def create?
-    false
-  end
-
-  def new?
-    create?
-  end
-
-  def update?
-    false
-  end
-
-  def edit?
-    update?
-  end
-
-  def destroy?
-    false
-  end
-
   def scope
     Pundit.policy_scope!(user, record.class)
   end
