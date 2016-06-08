8aa65621376ce0eb3bdff970983c155db5e22cff
Mohammad Alow <md-alow@hotmail.com>
2016-06-08 09:59:52 +0300

    Redirect a signed in user to dashboard when he visits home page.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
   def home
+    if signed_in?
+      redirect_to user_root_url
+    end
   end
 
   def about
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,19 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
+  describe 'GET #home' do
+    context 'guest user' do
+      it 'succeed' do
+        get :home
+        expect(response).to have_http_status :success
+      end
+    end
 
-  describe "GET #home" do
-    it "returns http success" do
-      get :home
-      expect(response).to have_http_status(:success)
+    context 'signed in user' do
+      it 'redirects to dashboard' do
+        sign_in create(:user)
+        get :home
+        expect(response).to redirect_to user_root_url
+      end
     end
   end
 
-  describe "GET #about" do
-    it "returns http success" do
+  describe 'GET #about' do
+    it 'succeed' do
       get :about
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status :success
     end
   end
-
 end
