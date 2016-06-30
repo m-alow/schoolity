e4c3430f002789b303adbaee38678088e4c74700
Mohammad Alow <md-alow@hotmail.com>
2016-06-30 11:19:02 +0300

    Move announcements_path from Schools::AnnouncementsHelper to AnnouncementsHelper.

--- app/helpers/announcements_helper.rb
+++ app/helpers/announcements_helper.rb
@@ -1,2 +1,11 @@
 module AnnouncementsHelper
+  def announcements_path announcement
+    if announcement.for_school?
+      school_announcements_path announcement.announceable
+    elsif announcement.for_school_class?
+      school_class_announcements_path announcement.announceable
+    elsif announcement.for_classroom?
+      classroom_announcements_path announcement.announceable
+    end
+  end
 end
