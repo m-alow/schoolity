927f7d72cd24111ffbf2dcb95123d66cc01f7a98
Mohammad Alow <md-alow@hotmail.com>
2016-07-01 19:06:26 +0300

    Fix bug: teacher does not teach a subject if it is not present.

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,6 +58,7 @@ class User < ActiveRecord::Base
   end
 
   def teaches_subject_in_classroom? subject, classroom
+    return false unless subject
     teachings.exists?(classroom: classroom, subject: subject) || teachings.exists?(classroom: classroom, all_subjects: true)
   end
 
