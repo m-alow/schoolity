a2bbd6e72fe04f90a3eb64f85129803e6c28729d
Mohammad Alow <md-alow@hotmail.com>
2016-07-04 23:24:31 +0300

    Add agenda on a day to parent panel.

--- app/controllers/parent/agendas_controller.rb
+++ app/controllers/parent/agendas_controller.rb
@@ -1,3 +1,5 @@
+require_dependency 'day/on_date'
+
 class Parent::AgendasController < ApplicationController
   before_action :set_following
 
@@ -9,6 +11,25 @@ class Parent::AgendasController < ApplicationController
   # GET /parent/followings/1/agendas/2010-10-10
   def show
     authorize @following
+
+    @date = params[:date].to_date
+    @classroom = @following.student.classroom
+    unless @classroom.present?
+      flash.now[:alert] = 'No classroom'
+      return render(:missing)
+    end
+
+    day_result = DayOnDate.new.call @classroom, @date
+    case day_result.status
+    when :study_day
+      @day = day_result.day
+    when :weekend
+      flash.now[:alert] = 'Weekend'
+      render :missing, notice: 'Weekend'
+    when :no_timetable
+      flash.now[:alert] = 'No Timetable'
+      render :missing
+    end
   end
 
   private
