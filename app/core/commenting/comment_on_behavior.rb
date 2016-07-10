dd531c3620a764e03cecfeb0ef873de1f86b69e0
Mohammad Alow <md-alow@hotmail.com>
2016-07-10 13:41:40 +0300

    Add comment on day behavior.

--- app/core/commenting/comment_on_behavior.rb
+++ app/core/commenting/comment_on_behavior.rb
@@ -20,6 +20,10 @@ module Commenting
     end
 
     def self.user_role user, behavior
+      send "#{behavior.behaviorable_type.underscore}_user_role", user, behavior
+    end
+
+    def self.lesson_user_role user, behavior
       classroom = behavior.behaviorable.day.classroom
       if user.owns? classroom.school
         'School Owner'
@@ -27,6 +31,17 @@ module Commenting
         'School Admin'
       elsif user.teaches_subject_in_classroom? behavior.behaviorable.subject, classroom
         'Teacher'
+      elsif user.follows? behavior.student
+        'Parent'
+      end
+    end
+
+    def self.day_user_role user, behavior
+      classroom = behavior.behaviorable.classroom
+      if user.owns? classroom.school
+        'School Owner'
+      elsif user.administrates? classroom.school
+        'School Admin'
       elsif user.follows? behavior.student
         'Parent'
       end
