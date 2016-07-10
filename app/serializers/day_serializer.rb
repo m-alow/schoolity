eac4fdc06e8e9348c4b536d72a529e24a2e219d7
Mohammad Alow <md-alow@hotmail.com>
2016-07-10 13:46:40 +0300

    Add behavior to day via json api.

--- app/serializers/day_serializer.rb
+++ app/serializers/day_serializer.rb
@@ -1,6 +1,10 @@
 class DaySerializer < ActiveModel::Serializer
   has_many :lessons
-  attributes :date, :content_type, :content, :classroom, :lessons
+  attributes :date, :content_type, :content, :behavior, :classroom, :lessons
+
+  def behavior
+    ActiveModelSerializers::SerializableResource.new(object.behaviors.find_by student_id: student_id)
+  end
 
   def classroom
     {
@@ -13,5 +17,11 @@ class DaySerializer < ActiveModel::Serializer
 
   def lessons
     object.lessons.sort_by(&:order)
+  end
+
+  private
+
+  def student_id
+    instance_options[:student_id]
   end
 end
