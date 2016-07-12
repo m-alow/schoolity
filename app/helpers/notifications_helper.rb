ea220f2ad5f5b85d6d5e4f8ca226c845142b32ff
Mohammad Alow <md-alow@hotmail.com>
2016-07-12 13:10:09 +0300

    Make discussion links in feed redirect to notification.

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,11 @@
 module NotificationsHelper
+  def notifiable_link notification
+    c = notification.notifiable.comments.count
+    link_to('Discuss', notification) +
+      unless c.zero?
+        ' ' + content_tag(:span, c, class: 'badge')
+      else
+        ''
+      end.html_safe
+  end
 end
