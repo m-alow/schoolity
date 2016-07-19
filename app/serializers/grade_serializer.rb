0b23a37d9226ac9ebbe9a893cbbce8b379125d8f
Mohammad Alow <md-alow@hotmail.com>
2016-07-20 00:30:07 +0300

    Add student to grade serializer.

--- app/serializers/grade_serializer.rb
+++ app/serializers/grade_serializer.rb
@@ -1,7 +1,11 @@
 class GradeSerializer < ActiveModel::Serializer
-  attributes :id, :score, :exam
+  attributes :id, :score, :exam, :student
 
   def exam
     ActiveModelSerializers::SerializableResource.new(object.exam)
+  end
+
+  def student
+    object.student.name
   end
 end
