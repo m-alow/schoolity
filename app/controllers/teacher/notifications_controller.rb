f6125977acf635ca610345c1130436b4ec3ffbe0
Mohammad Alow <md-alow@hotmail.com>
2016-07-20 21:41:34 +0300

    Paginate notifications.

--- app/controllers/teacher/notifications_controller.rb
+++ app/controllers/teacher/notifications_controller.rb
@@ -2,10 +2,10 @@ class Teacher::NotificationsController < ApplicationController
   include EnsureTeacher
 
   def index
-    @notifications = current_user.teacher_notifications.unread
+    @notifications = current_user.teacher_notifications.unread.paginate(page: params[:page])
   end
 
   def all
-    @notifications = current_user.teacher_notifications.sorted
+    @notifications = current_user.teacher_notifications.sorted.paginate(page: params[:page])
   end
 end
