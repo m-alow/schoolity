da423f45dfcd8babf5bff1a58a16f8db10b3aa08
Mohammad Alow <md-alow@hotmail.com>
2016-07-23 00:04:21 +0300

    Enable notifications when a user logs in.

--- app/controllers/api/v1/parent/tokens_controller.rb
+++ app/controllers/api/v1/parent/tokens_controller.rb
@@ -4,9 +4,13 @@ module Api
       class TokensController < ApiController
         # POST /api/v1/parent/tokens
         def create
-          token = DeviceToken.create user: current_user,
-                                     token: params[:token],
-                                     role: 'Follower'
+          token = DeviceToken
+                  .find_or_initialize_by(user: current_user,
+                                         token: params[:token],
+                                         role: 'Follower')
+                  .tap { |token| token.enabled = true }
+                  .save
+
           respond_to do |format|
             format.json { render json: { token: params[:token] }, status: :ok }
           end
