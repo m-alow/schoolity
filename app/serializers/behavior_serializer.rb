15df6f5dd074142745f53deb152dab5466a6de5a
Mohammad Alow <md-alow@hotmail.com>
2016-07-25 21:32:53 +0300

    Add behaviorable to behavior via json api.

--- app/serializers/behavior_serializer.rb
+++ app/serializers/behavior_serializer.rb
@@ -1,6 +1,7 @@
 class BehaviorSerializer < ActiveModel::Serializer
   belongs_to :student
-  attributes :id, :content_type, :content, :comments_count
+  belongs_to :behaviorable
+  attributes :id, :content_type, :content, :comments_count, :behaviorable_type
 
   def comments_count
     object.comments.count
